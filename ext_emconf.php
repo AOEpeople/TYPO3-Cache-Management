d24c98ffa36d50cd870c1a572db025032c0ad972
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2017-05-10 16:38:36 +0200

    [CLEANUP] Update extension manager configuration
    - Fix whitespaces.
    - Remove redundant configuration.

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,35 +11,33 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Cache Management Extension',
-	'description' => 'Provides management of the page caching for high traffic websites.',
-	'category' => 'module',
-	'author' => 'Kasper Skaarhoj',
-	'author_email' => 'kasper@typo3.com',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => 'mod',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'author_company' => '',
-	'version' => '2.5.1',
-	'_md5_values_when_last_written' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'suggests' => array(
-	),
+    'title' => 'Cache Management Extension',
+    'description' => 'Provides management of the page caching for high traffic websites.',
+    'category' => 'module',
+    'author' => 'Kasper Skaarhoj',
+    'author_email' => 'kasper@typo3.com',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => 'mod',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'author_company' => '',
+    'version' => '2.5.1',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-7.6.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    '_md5_values_when_last_written' => '',
 );
