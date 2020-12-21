69425c581a0e3aacfb00e564e0345dfe89552e37
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-12-21 15:17:15 +0100

    [TASK] Skip tests with respect to TYPO3 Version

--- Tests/Functional/Domain/Repository/CacheTableRepositoryTest.php
+++ Tests/Functional/Domain/Repository/CacheTableRepositoryTest.php
@@ -28,6 +28,7 @@ namespace Aoe\Cachemgm\Tests\Functional\Domain\Repository;
 
 use Aoe\Cachemgm\Domain\Repository\CacheTableRepository;
 use Nimut\TestingFramework\TestCase\FunctionalTestCase;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 
@@ -51,10 +52,33 @@ class CacheTableRepositoryTest extends FunctionalTestCase
      */
     public function countRowsInTable()
     {
+        $version = (new Typo3Version())->getMajorVersion();
+        if($version === 10) {
+            $this->markTestSkipped('This test is only valid for TYPO3 10 LTS');
+        }
+
         $this->importDataSet(__DIR__ . '/Fixtures/cf_cache_pages.xml');
         $this->assertEquals(
             2,
             $this->subject->countRowsInTable('cf_cache_pages')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function countRowsInTableV10()
+    {
+
+        $version = (new Typo3Version())->getMajorVersion();
+        if($version !== 10) {
+            $this->markTestSkipped('This test is only valid for TYPO3 10 LTS');
+        }
+
+        $this->importDataSet(__DIR__ . '/Fixtures/cache_pages.xml');
+        $this->assertEquals(
+            2,
+            $this->subject->countRowsInTable('cache_pages')
         );
     }
 }
