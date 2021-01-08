9d4808d64e300c152356a40c4438a78cc4e04ebe
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2021-01-08 12:34:36 +0100

    [CI] Switch to GitHub actions (#8)

--- Tests/Functional/Domain/Repository/CacheTableRepositoryTest.php
+++ Tests/Functional/Domain/Repository/CacheTableRepositoryTest.php
@@ -51,10 +51,41 @@ class CacheTableRepositoryTest extends FunctionalTestCase
      */
     public function countRowsInTable()
     {
+        if($this->isTYPO310LTS()) {
+            $this->markTestSkipped('This test is only valid for TYPO3 8 and 9 LTS');
+        }
+
         $this->importDataSet(__DIR__ . '/Fixtures/cf_cache_pages.xml');
         $this->assertEquals(
             2,
             $this->subject->countRowsInTable('cf_cache_pages')
         );
+    }
+
+    /**
+     * @test
+     */
+    public function countRowsInTableV10()
+    {
+        if(!$this->isTYPO310LTS()) {
+            $this->markTestSkipped('This test is only valid for TYPO3 10 LTS');
+        }
+
+        $this->importDataSet(__DIR__ . '/Fixtures/cache_pages.xml');
+        $this->assertEquals(
+            2,
+            $this->subject->countRowsInTable('cache_pages')
+        );
+    }
+
+    private function isTYPO310LTS(): bool
+    {
+        if(
+            class_exists(\TYPO3\CMS\Core\Information\Typo3Version::class)
+            && (new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() === 10
+        ) {
+            return true;
+        }
+        return false;
     }
 }
