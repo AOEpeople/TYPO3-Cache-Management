af3c33db4fac8f503c66fdaa3d23c4828ab26e67
thomas.layh <thomas.layh@aoe.com>
2022-02-24 10:56:19 +0100

    [TASK] fix ext_emconf version dependencies and raise version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '10.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
