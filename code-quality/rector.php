04b8c3f069bef54d53301a1406ebbd4a88264b08
thomas.layh <thomas.layh@aoe.com>
2022-09-29 08:34:40 +0200

    [TASK] remove commented rector excludes

--- code-quality/rector.php
+++ code-quality/rector.php
@@ -86,46 +86,6 @@ return static function (ContainerConfigurator $containerConfigurator): void
             RepeatedLiteralToClassConstantRector::class,
             RemoveUnusedVariableInCatchRector::class,
             RemoveUnusedVariableInCatchRector::class,
-            /*
-            ChangeOrIfReturnToEarlyReturnRector::class,
-            ChangeAndIfToEarlyReturnRector::class,
-            TypedPropertyRector::class => [
-                __DIR__ . '/../Classes/Domain/Model/Tag/PageIdTag.php',
-                __DIR__ . '/../Tests/Unit/TYPO3/Hooks/ContentPostProcOutputHookTest.php',
-            ],
-
-
-            RecastingRemovalRector::class,
-            ConsistentPregDelimiterRector::class,
-            PostIncDecToPreIncDecRector::class,
-            ReturnBinaryAndToEarlyReturnRector::class,
-            MakeBoolPropertyRespectIsHasWasMethodNamingRector::class,
-            MoveOutMethodCallInsideIfConditionRector::class,
-            ReturnArrayClassMethodToYieldRector::class,
-            AddArrayParamDocTypeRector::class,
-            AddArrayReturnDocTypeRector::class,
-            ReturnFluentChainMethodCallToNormalMethodCallRector::class,
-            IssetOnPropertyObjectToPropertyExistsRector::class,
-            FlipTypeControlToUseExclusiveTypeRector::class,
-            RenameVariableToMatchNewTypeRector::class,
-            AddLiteralSeparatorToNumberRector::class,
-            RenameForeachValueVariableToMatchMethodCallReturnTypeRector::class,
-            ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
-            PrivatizeLocalPropertyToPrivatePropertyRector::class,
-            RemoveDelegatingParentCallRector::class,
-            */
-
-
-            // @todo strict php
-            /*
-            ArgumentAdderRector::class,
-            ParamTypeDeclarationRector::class,
-            ReturnTypeDeclarationRector::class,
-            JsonThrowOnErrorRector::class,
-            AddArrayDefaultToArrayPropertyRector::class,
-            SimplifyIfReturnBoolRector::class,
-            SimplifyBoolIdenticalTrueRector::class,
-            */
         ]
     );
 
